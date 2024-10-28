88258731c2fc4c4bd35b68a58867a57bf19ae218
Ali Lazraq <a.lazraq@aui.ma>
2024-10-28 03:03:18 +0100

    Updated theme.js and Topbar to implement dark/light mode toggle + started on sidebar.js

---
+++ src/scenes/global/Sidebar.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+import { ProSidebar, Menu, MenuItem} from 'react-pro-sidebar';
+import 'react-pro-sidebar/dist/css/styles.css';
+import { Box, IconButton, UseTheme, Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
+import { tokens } from '../../theme';
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import PeopleOutlinedIcon from '@mui/icons-material/PeopleOutlined';
+import ContactsOutlinedIcon from '@mui/icons-material/ContactsOutlined';
+import ReceiptOutlinedIcon from '@mui/icons-material/ReceiptOutlined';
+import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
+import CalenderTodayOutlinedIcon from '@mui/icons-material/CalenderTodayOutlined';
+import HelpOutlinedIcon from '@mui/icons-material/HelpOutlined';
+import BarchartOutlinedIcon from '@mui/icons-material/BarchartOutlined';
+import PieChartOutlineOutlinedIcon from '@mui/icons-material/PieChartOutlineOutlined';
+import TimelineOutlinedIcon from '@mui/icons-material/TimelineOutlined';
+import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
+import MapOutlinedIcon from '@mui/icons-material/MapOutlined';
+
+
+const Sidebar = () => {
+    return (
+        <div> Sidebar </div>
+    );
+    }
+
+export default Sidebar;
